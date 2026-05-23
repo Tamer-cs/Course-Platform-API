47a4c6665b56ac578dab746cc3a0990ac67d6bd0
Tamer-cs <tameralafeef1@gmail.com>
2026-05-23 20:14:29 +0300

    force https instead of http

--- src/main/java/com/courseplatform/api/ApiApplication.java
+++ src/main/java/com/courseplatform/api/ApiApplication.java
@@ -1,13 +1,30 @@
 package com.courseplatform.api;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server()
+                                .url("https://course-platform-api-production-6d24.up.railway.app")
+                                .description("Production Server (HTTPS)"),
+                        new Server()
+                                .url("http://localhost:8081")
+                                .description("Local Development Server")
+                ));
+    }
 }
